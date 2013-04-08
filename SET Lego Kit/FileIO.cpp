112ca605c6ddde1be8e2d3f6cbab295abf237ea4
Ian Campbell <campbell@Ians-Mac-mini.local>
2013-04-08 02:12:26 -0400

    adding files to repo

---
+++ SET Lego Kit/FileIO.cpp
@@ -0,0 +1,9 @@
+//
+//  FileIO.cpp
+//  SET Lego Kit
+//
+//  Created by Ian Campbell on 2013-04-08.
+//  Copyright (c) 2013 Ian Campbell. All rights reserved.
+//
+
+#include "FileIO.h"
---
+++ SET Lego Kit/FileIO.h
@@ -0,0 +1,25 @@
+//
+//  FileIO.h
+//  SET Lego Kit
+//
+//  Created by Ian Campbell on 2013-04-08.
+//  Copyright (c) 2013 Ian Campbell. All rights reserved.
+//
+
+#ifndef __SET_Lego_Kit__FileIO__
+#define __SET_Lego_Kit__FileIO__
+
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+class FileIO{
+private:
+    //error condition integer
+    int errorLevel;
+    
+public:
+    
+};
+
+#endif /* defined(__SET_Lego_Kit__FileIO__) */
