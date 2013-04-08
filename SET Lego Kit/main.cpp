cca1817fb5b6dc31bbb8bdb56e7c12a912aa6f1b
Ian Campbell <campbell@Ians-Mac-mini.local>
2013-04-08 01:32:23 -0400

    Initial Commit

---
+++ SET Lego Kit/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SET Lego Kit
+//
+//  Created by Ian Campbell on 2013-04-08.
+//  Copyright (c) 2013 Ian Campbell. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
